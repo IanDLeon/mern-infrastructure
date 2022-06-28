3ee7e4d3c9e4f1ba39fbe114e7822c1de63ab80a
IanDLeon <ian.reggie.deleon@gmail.com>
2022-06-28 16:58:26 -0400

    Auts Page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,11 @@
-export default function AuthPage() {
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+export default function AuthPage({setUser}) {
  return (
-    <h1>Auth Page</h1>
+   <main>
+     <h1>Auth Page</h1>
+     <SignUpForm setUser={setUser}/>
+     <LoginForm setUser={setUser} />
+   </main>
   );
 }
